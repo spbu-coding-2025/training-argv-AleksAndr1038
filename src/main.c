ed6b8f3ec257dbb8fb023e88ea53a3e929f80ad6
Александр Смоликов <aleksandrsmolikov107@gmail.com>
2025-10-17 22:30:23 +0300

    Обработка аргументов из стандартного потока ввода

---
+++ src/main.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+	int lower_bound = 0;
+	int upper_bound = 0;
+	int lower_bound_check = 0;
+	int upper_bound_check = 0;
+
+	for (int i = 0; i < argc; i++) {
+		if (strcmp(argv[i], "--lower_bound=", 14) == 0) {
+			lower_bound = atoi(argv[i] + 14);
+			lower_bound_check = 1;
+		}
+		else if (strcmp(argv[i], "--upper_bound=", 14) == 0) {
+			upper_bound = atoi(argv[i] + 14);
+			upper_bound_check = 1;
+		}
+	}
+
+	if (!lower_bound_check) {
+		fprintf(stderr, "Flag --lower_bound is missing\n");
+		return 1;
+	}
+
+	if (!upper_bound_check) {
+		fprintf(stderr, "Flag --upper_bound is missing\n");
+		return 1;
+	}
+
+	if (!lower_bound_check && !upper_bound_check) {
+		fprintf(stderr, "No flags found\n");
+		return 1;
+	}
+
+	if (lower_bound > upper_bound) {
+		fprintf(stderr, "The lower bound is greater than the upper bound\n");
+		return 1;
+	}
+
+	for (int i = lower_bound; i <= upper_bound; i++) {
+		printf("%d ", i);
+	}
+
+	return 0;
+}
